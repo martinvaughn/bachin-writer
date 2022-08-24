2bd04c85cc840b4ab07dca0a57bd1f13dadc8169
Martin Vaughn <martinvaughn.io@gmail.com>
2022-08-24 09:24:45 -1000

    Change tingz

--- bachin-writer.py
+++ bachin-writer.py
@@ -16,9 +16,9 @@ def get_folder(char: str, idx: int):
         num = get_random_number(_path)
         return ("char", _path + "/lower_" + char + "_" + str(num) + ".svg", idx)
     if char.isupper():
-        _path = "svgs/upper/upper_" + char
+        _path = "svgs/upper/upper_" + char.lower()
         num = get_random_number(_path)
-        return ("char", _path + "/upper_" + char + "_" + str(num) + ".svg", idx)
+        return ("char", _path + "/upper_" + char.lower() + "_" + str(num) + ".svg", idx)
     #### Individual Checks
     if char == " ":
         return ("space", "svgs/blank_template_7_x_10.svg", idx)
@@ -72,81 +72,13 @@ def get_folder(char: str, idx: int):
 
 def main():
     doc = ss.Document()
-    input_str = "hello my name's what my name's who my name is slim shaddy. ~Please keep all hands and feet in the boat at all times and do not lose your balance because there are certainly sharks and sharks are not your friend no matter how cute you think they are, remember that all they see is a delicious human sived snack. don't @ me tho ~new line added here. 12 04 89~112244008899"
+    input_str = "hello my name's what my name's who my name is slim shaddy. ~Please keep all hands and feet in the boat at all times and do not lose your balance because there are certainly sharks and sharks are not your friend no matter how cute you think they are, remember that all they see is a delicious human sived snack. don't @ me tho ~new line added here. 12 04 89~112244008899. CloCK PRawn"
     ftuples = []
     for idx, char in enumerate(input_str):
         folder_tuple = get_folder(char, idx)
         print(folder_tuple)
         ftuples.append(folder_tuple)
-
-    ftuples2 = [
-            ("char","svgs/lower/lower_h/lower_h_1.svg",1), 
-            ("char","svgs/lower/lower_i/lower_i_1.svg",2), 
-            ("space","svgs/blank_template.svg",3),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",4),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",5), 
-            ("char","svgs/lower/lower_s/lower_s_1.svg",6),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",7), 
-            ("space","svgs/blank_template.svg",8),
-            # ("new line", 1),
-            ("char","svgs/lower/lower_m/lower_m_1.svg",9),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",10),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",11),
-            ("char","svgs/lower/lower_n/lower_n_1.svg",12),
-            ("space","svgs/blank_template.svg",88),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",13),
-            ("char","svgs/lower/lower_n/lower_n_1.svg",14),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",15),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",16),
-            ("space","svgs/blank_template.svg",90),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",17),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",18),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",19),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",20),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",21),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",22),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",23),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",24),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",25),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",26),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",27),
-            ("new line","svgs/blank_template.svg",28),
-            ("space","svgs/blank_template.svg",36),
-            ("char","svgs/lower/lower_h/lower_h_1.svg",29), 
-            ("char","svgs/lower/lower_i/lower_i_1.svg",30), 
-            ("space","svgs/blank_template.svg",37),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",31),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",32), 
-            ("char","svgs/lower/lower_s/lower_s_1.svg",33),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",34), 
-            ("space","svgs/blank_template.svg",84),
-            ("char","svgs/lower/lower_m/lower_m_1.svg",35),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",36),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",38),
-            ("char","svgs/lower/lower_n/lower_n_1.svg",39),
-            ("space","svgs/blank_template.svg",89),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",40),
-            ("char","svgs/lower/lower_n/lower_n_1.svg",41),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",42),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",43),
-            ("space","svgs/blank_template.svg",85),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",44),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",45),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",46),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",47),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",48),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",49),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",50),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",51),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",52),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",53),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",54),
-            ("space","svgs/blank_template.svg",101),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",55),
-            ("char","svgs/lower/lower_n/lower_n_1.svg",56),
-            ("char","svgs/lower/lower_o/lower_o_1.svg",57),
-            ("char","svgs/lower/lower_s/lower_s_1.svg",58),
-            ]
+ 
     result_layout = ss.VBoxLayout()
     yes = True
     while yes == True:
